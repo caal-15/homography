2c6e26d4118b2e95768a0435617718ffaa517aea
Carlos Gonzalez <caal.0522@gmail.com>
2017-09-10 19:07:38 -0500

    Added image transplaning using round_method, interpolation not implemented yet

--- homography.py
+++ homography.py
@@ -1,4 +1,6 @@
 import numpy as np
+import cv2
+import os
 
 
 def compute_homography(x, x_new):
@@ -19,7 +21,31 @@ def compute_homography(x, x_new):
     return h_vec.reshape(3, 3)
 
 
-x = [(514, 252), (714, 252), (514, 402), (714, 402)]
-x_new = [(514, 252), (739, 229), (523, 458), (747, 845)]
+def transplane_point(point, homography):
+    preliminary = homography.dot(point)
+    return preliminary / preliminary[2]
 
-print(compute_homography(x, x_new))
+
+def transplane_image(from_image, to_image, homography, outpath):
+    to_height, to_width, to_channels = to_image.shape
+    from_height, from_width, from_channels = from_image.shape
+    for i in range(0, to_width):
+        for j in range(0, to_height):
+            trans_point = transplane_point(np.array([i, j, 1]), homography)
+            new_i = int(round(trans_point[0]))
+            new_j = int(round(trans_point[1]))
+            cond_1 = new_i < from_width and new_i > 0
+            cond_2 = new_j < from_height and new_j > 0
+            if cond_1 and cond_2:
+                to_image[j][i] = from_image[new_j][new_i]
+    cv2.imwrite(outpath, to_image)
+
+
+x = [(514, 252), (714, 252), (514, 402), (714, 402)]
+x_new = [(514, 252), (739, 229), (523, 458), (747, 485)]
+
+H = compute_homography(x, x_new)
+from_image = cv2.imread(os.path.dirname(__file__).join('picture.jpg'))
+to_image = np.zeros((from_image.shape[0], from_image.shape[1], 3), np.uint8)
+
+transplane_image(from_image, to_image, H, 'transplaned.png')
