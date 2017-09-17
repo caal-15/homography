c4014c474b5d2cd884e11f016b9ce30e09ce55db
Carlos Gonzalez <caal.0522@gmail.com>
2017-09-17 18:20:29 -0500

    Added comments

--- homography/homography.py
+++ homography/homography.py
@@ -3,8 +3,17 @@ from math import floor
 
 
 def compute_homography(x, x_new):
+    '''
+        Computes the homography matrix between two planes given two arrays
+        of points that correspond between them in both planes.
+
+        x: Array of points on the original plane.
+        x_new: Array of points that x map to on the destiantion plane.
+    '''
     input_mat = []
     output_vec = []
+    # Organize the input points into the matrix and the vector necessary to
+    # solve the system
     for i in range(0, 4):
         input_mat.append([
             x[i][0], x[i][1], 1, 0, 0, 0, -(x[i][0] * x_new[i][0]),
@@ -14,18 +23,38 @@ def compute_homography(x, x_new):
             -(x[i][1] * x_new[i][1])])
         output_vec.append(x_new[i][0])
         output_vec.append(x_new[i][1])
+    # Create numpy arrays from the organized data
     np_input_mat = np.array(input_mat)
     np_output_vec = np.array(output_vec)
+    # Obtain values for h11 through h32, append a 1 at the end
     h_vec = np.append(np.linalg.solve(np_input_mat, np_output_vec), [1])
+    # Reshape into 3x3 matrix and return
     return h_vec.reshape(3, 3)
 
 
 def transplane_point(point, homography):
+    '''
+        Transplanes a point given the an homography matrix.
+
+        point: array of the form [x, y, 1].
+        homography: 3x3 numpy array.
+    '''
+    # Transplane point: x' = xH
     preliminary = homography.dot(point)
+    # Divide by the last component to ensure the form [x', y', 1]
     return preliminary / preliminary[2]
 
 
 def rounded_approximation(i, j, from_image):
+    '''
+        Returns the pixel on the position round(i), round(j), from the provided
+        image, if the pixels are negative or exceed the boundaries of the image
+        it returns None.
+
+        i: float column
+        j: float row
+        from_image: image to take the pixels from
+    '''
     from_height, from_width, from_channels = from_image.shape
     new_i = int(round(i))
     new_j = int(round(j))
@@ -38,6 +67,16 @@ def rounded_approximation(i, j, from_image):
 
 
 def interpolated_approximation(i, j, from_image):
+    '''
+        Returns a 3 position array containing the interpolated_approximation
+        of the colors for float coordinates i and j using the 4 surrounding
+        pixels, if either i, or j, or their respective surrounding pixels are
+        negative or exceed the boundaries of the image, it returns None.
+
+        i: float column
+        j: float row
+        from_image: image to take the pixels from
+    '''
     from_height, from_width, from_channels = from_image.shape
     if i.is_integer() and j.is_integer():
         return from_image[int(j)][int(i)]
@@ -65,17 +104,29 @@ def interpolated_approximation(i, j, from_image):
 
 
 def transplane_image(from_image, to_image, homography, interpolate=False):
+    '''
+        Transplanes one image to the plane of the other given the right
+        homography, it does so by the rounding method, unless the interpolation
+        parameter is True, returns the transplaned image.
+
+        from_image: source image to transplane.
+        to_image: image to transplane source onto.
+        homography: 3x3 numpy array.
+        interpolate: Boolean to determine the use of bi-linear interpolation.
+    '''
     to_height, to_width, to_channels = to_image.shape
-    from_height, from_width, from_channels = from_image.shape
     for i in range(0, to_width):
         for j in range(0, to_height):
+            # Transplane each point of the to_image
             trans_point = transplane_point(np.array([i, j, 1]), homography)
+            # If specified, use bi-linear interpolation
             if interpolate:
                 new_point = interpolated_approximation(
                     trans_point[0], trans_point[1], from_image)
             else:
                 new_point = rounded_approximation(
                     trans_point[0], trans_point[1], from_image)
+            # if a match was found update the pixel of to_image
             if new_point is not None:
                 to_image[j][i] = new_point
     return to_image
--- run.py
+++ run.py
@@ -5,37 +5,53 @@ from homography import compute_homography, transplane_image
 
 
 if __name__ == '__main__':
+    # Correspondences between original image and
+    # image with projectivity removed
     x = [(514, 252), (714, 252), (514, 402), (714, 402)]
     x_new = [(514, 252), (739, 229), (523, 458), (747, 485)]
+    # Compute the homography
     H = compute_homography(x, x_new)
 
+    # Load the original image and Create the destination image
     original_image = cv2.imread(os.path.dirname(__file__).join('picture.jpg'))
     corrected_image = np.zeros(
         (original_image.shape[0], original_image.shape[1], 3), np.uint8)
 
+    # Transplane image to remove projectivity and write to file
+    # (without interpolation)
     cv2.imwrite('transpalaned.png', transplane_image(
         original_image, corrected_image, H))
+    # Transplane image to remove projectivity and write to file
+    # (with interpolation)
     cv2.imwrite(
         'transplaned_interpoplated.png',
         transplane_image(original_image, corrected_image, H, True))
 
+    # Correspondences between picture of paper, and scanner simulated one
     x = [(0, 0), (773, 0), (0, 1000), (773, 1000)]
     x_new = [(156, 656), (2072, 656), (156, 3176), (2072, 3176)]
+    # Compute the homography
     H = compute_homography(x, x_new)
 
+    # Load the paper image and Create the destination image
     paper_image = cv2.imread(os.path.dirname(__file__).join('paper.jpg'))
     scanned_paper = np.zeros((1000, 773, 3), np.uint8)
 
+    # Transplane image to simulate scanner and write to file
     cv2.imwrite(
         'scanned_paper.png',
         transplane_image(paper_image, scanned_paper, H, True))
 
+    # Correspondences between straight image and original image
     x = [(535, 275), (712, 262), (542, 435), (722, 452)]
     x_new = [(0, 0), (546, 0), (0, 489), (546, 489)]
+    # Compute the homography
     H = compute_homography(x, x_new)
 
+    # Load staright image
     sign_image = cv2.imread(os.path.dirname(__file__).join('new_sign.png'))
 
+    # Transplane straight image to add projectivity and write to file
     cv2.imwrite(
         'new_sign_transplaned.png',
         transplane_image(sign_image, original_image, H, True))
